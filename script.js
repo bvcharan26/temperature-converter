dd6220e1de4740c099d18dfbefc0532004c3b856
B V CHARAN KUMAR <120728597+bvcharan26@users.noreply.github.com>
2023-10-03 14:27:07 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,10 +1,29 @@
 function convert(){
-    var opt=document.getElementById('conv').value;
-    if(opt==='1'){
-        document.getElementById('input1').innerText="Enter Temperatue in Kelvin";
-        var temp=parseFloat(document.getElementById('one').value);
-        var a=273.15;
-        var an=temp-a;
-        document.getElementById('ans').innerText=an.toFixed(2);
+    var num1=parseFloat(document.getElementById('one').value);
+    var oper=document.getElementById('conv').value;
+    var ans;
+    if(oper=='1'){
+        ans=(num1)-273.15;
+        document.getElementById('answer').value=ans.toFixed(2)+" "+"Degrees Celcius";
+    }
+    if(oper=='2'){
+        ans=num1+273.15;
+        document.getElementById('answer').value=ans.toFixed(2)+" "+"Degrees Kelvin"
+    }
+    if(oper=='3'){
+        ans=(num1-32)/1.8;
+        document.getElementById('answer').value=ans.toFixed(2)+" "+"Degrees Celsius";
+    }
+    if(oper=='4'){
+        ans=(num1*1.8)+32;
+        document.getElementById('answer').value=ans.toFixed(2)+" "+"Degrees Fahrenheit";
+    }
+    if(oper=='5'){
+        ans=(num1-32)*(5/9)+273.15;
+        document.getElementById('answer').value=ans.toFixed(2)+" "+"Degrees Kelvin";
+    }
+    if(oper=='6'){
+        ans=(num1-273.15)*(9/5)+32;
+        document.getElementById('answer').value=ans.toFixed(2)+" "+"Degrees Fahrenheit";
     }
 }
